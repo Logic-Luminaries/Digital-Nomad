dbdf28662a8f7b878ad5018d8ac0225e6935bcfc
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-09-04 11:02:23 +0200

    Changed register

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Login from './login';
 import './login.css';
+import Register from './register';
+import './register.css';
 
 
 function App() {
   return (
       <div className="App">
-      <Login />
+      <Register />
       </div>
   );
 }
