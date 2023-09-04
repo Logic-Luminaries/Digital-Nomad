98cddc61df2cb061376a583c60cf5a970bfdeff3
Mikaeel Suffla <mikaeel.suffla@gmail.com>
2023-09-04 09:40:30 +0200

    Map and Filter buttons

---
+++ src/filter.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const filterButton = document.getElementById('filterButton');
+
+    filterButton.addEventListener('click', () => {
+        
+        alert();
+    });
+});
---
+++ src/map.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const mapButton = document.getElementById('mapButton');
+    const mapContainer = document.getElementById('mapContainer');
+
+    mapButton.addEventListener('click', () => {
+        
+        alert();
+    });
+});
