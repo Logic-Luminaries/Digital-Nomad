c046565d4f7d9e7ef3acd903528134a1ba006bb7
Mikaeel Suffla <mikaeel.suffla@gmail.com>
2023-09-04 13:42:18 +0200

    Completed Map and Filter Buttons

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Login from './login'; // Will need to route between pages
 import Register from './register'; // Will need to route between pages
 
 
+
 function App() {
   return (
     <div className="App">
-      <Register />
+      <MainPage />
     </div>
 
   );
--- src/mainpage.js
+++ src/mainpage.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import Login from './login';
 import Header from './header';
+import Header2 from './header2';
 import './mainpage.css'; //To style between components
 
 function MainPage   (){
   return (
     <div className="main-page">
       <Header />
-      <Login />
+      <Header2 />
 
     </div>
   );
