a513b762526663ef911d67ce7946fde880dfda27
Mikaeel Suffla <mikaeel.suffla@gmail.com>
2023-09-04 10:28:53 +0200

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from './login';
+import './login.css';
 
+
+function App() {
+  return (
+      <div className="App">
+      <Login />
+      </div>
+  );
+}
+
+export default App;
 
