a764d5fb8dcd44059195374ba2c7359ee6b4a274
Patrick Claassens <claassenspatrick1@gmail.com>
2023-09-08 15:08:25 +0200

    added dynamic adding to buildings, and styling

--- src/map.js
+++ src/map.js
@@ -2,11 +2,7 @@ import React from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import Header2 from "./header2";
 import Header from "./header";
-
-const containerStyle = {
-  width: "400px",
-  height: "400px",
-};
+import "./map.css";
 
 const center = {
   lat: -26.111,
@@ -15,9 +11,36 @@ const center = {
 
 //Placeholder for the pin's information
 const pin_info = [
-  { position: { lat: -26.111, lng: 28.049 }, title: "Building 1" },
-  { position: { lat: -15, lng: 30 }, title: "Building 2" },
-  { position: { lat: -30, lng: 10 }, title: "Building 3" },
+  {
+    position: { lat: -26.111, lng: 28.049 },
+    list_date: "2023-01-01",
+    type: "House",
+    beds: 10,
+    title: "Building 1",
+    price: 20000,
+    image:
+      "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwaG91c2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+  },
+  {
+    position: { lat: -15, lng: 30 },
+    list_date: "2023-08-13",
+    type: "cottage",
+    beds: 3,
+    title: "Building 2",
+    price: 230000,
+    image:
+      "https://people.com/thmb/Rq4-T9Jiu-hohH1zxcPFgdeszBQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(780x211:782x213)/barbie-Ken-malibu-Dream-House-Airbnb-tout-e18b10475a30478992ea81a023e8c1be.jpg",
+  },
+  {
+    position: { lat: -30, lng: 10 },
+    list_date: "2023-03-28",
+    type: "House",
+    beds: 8,
+    title: "Building 3",
+    price: 560000,
+    image:
+      "https://amazingarchitecture.com/storage/2184/brutalist_house_lyx_arkitekter_iceland.jpg",
+  },
 ];
 
 //Create markers and set them onto the map
@@ -27,28 +50,58 @@ function CreateMarker(positions, map) {
       position: item.position,
       title: item.title,
     });
-    bindInfoWindow(marker, map);
+    bindInfoWindow(
+      marker,
+      map,
+      item.title,
+      item.price,
+      item.image,
+      item.beds,
+      item.type,
+      item.list_date
+    );
     marker.setMap(map);
   });
 }
 
-function bindInfoWindow(marker, map) {
+function bindInfoWindow(
+  marker,
+  map,
+  title,
+  price,
+  image,
+  beds,
+  type,
+  list_date
+) {
   var infowindow = new window.google.maps.InfoWindow({
     content:
-      "<div class='map_info_wrapper'><div class='img_wrapper'><img src=https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwaG91c2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80></div>" +
+      "<div class='map_info_wrapper'><div class='img_wrapper'><img class=image src=" +
+      image +
+      "></div>" +
       "<div class='property_content_wrap'>" +
       "<div class='property_title'>" +
-      "<span>Testing Building</span>" +
+      "<span>" +
+      title +
+      "</span>" +
       "</div>" +
       "<div class='property_price'>" +
-      "<span>R200000</span>" +
+      "<span>R" +
+      price +
+      "</span>" +
       "</div>" +
       "<div class='property_bed_type'>" +
-      "<span>10</span>" +
-      "<ul><li>House</li></ul>" +
+      "<span>" +
+      beds +
+      "</span>" +
+      "<div>" +
+      type +
+      "</div>" +
       "</div>" +
       "<div class='property_listed_date'>" +
-      "<span>Listed on 12 October 2023</span>" +
+      "<span>Listed on " +
+      list_date +
+      "</span>" +
       "</div>" +
       "</div></a></div>",
   });
@@ -71,7 +124,6 @@ function MyComponent() {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-
     setMap(map);
 
     //Variable for the popup info window
@@ -89,11 +141,11 @@ function MyComponent() {
       <Header />
       <Header2 />
       <GoogleMap
-        mapContainerStyle={containerStyle}
         center={center}
         zoom={10}
         onLoad={onLoad}
         onUnmount={onUnmount}
+        mapContainerClassName="map-canvas"
       >
         <></>
       </GoogleMap>
