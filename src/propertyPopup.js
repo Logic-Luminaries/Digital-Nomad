567ff85fc18b294ada64c29b73f114c7245efb82
Mikaeel Suffla <mikaeel.suffla@gmail.com>
2023-09-19 12:15:25 +0200

    Added Desciption and Ammenties to properties

--- src/propertyPopup.js
+++ src/propertyPopup.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import './propertyPopup.css'; // Import the CSS file for styling
 
-
 function PropertyPopup({ property, onClose }) {
-    // This component receives the selected property as a prop and a function to close the popup
-  
-    return (
-      <div className="property-popup">
-        <div className="popup-content">
-          <button className="close-button" onClick={onClose}>
-            X
-          </button>
-          <div className="image-holder">
+  // This component receives the selected property as a prop and a function to close the popup
+
+  return (
+    <div className="property-popup">
+      <div className="popup-content">
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+        <div className="image-holder">
           <img src={property.imageUrl} alt={property.title} />
-          </div>
-          <h3>{property.title}</h3>
-          <p>{property.location}</p>
-          <p>{property.price}</p>
-          {/* Add more property details here */}
+        </div>
+        <h3>{property.title}</h3>
+        <p>{property.location}</p>
+        {/* Show description before amenities */}
+        <div className="description">
+          <h4>Description:</h4>
+          <p>{property.description}</p>
+        </div>
+        {/* Add Nearby section */}
+        <div className="nearby">
+          <h4>Nearby:</h4>
+          <ul>
+            {property.nearby.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        {/* Add amenities section */}
+        <div className="amenities">
+          <h4>Amenities:</h4>
+          <ul>
+            {property.amenities.map((amenity, index) => (
+              <li key={index}>{amenity}</li>
+            ))}
+          </ul>
         </div>
       </div>
-    );
-  }
-  
-  export default PropertyPopup;
+    </div>
+  );
+}
+
+export default PropertyPopup;
+
+
