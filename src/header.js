58940760c359714e436c20933761f9c81b4b4747
Muhammad Saif Alli <muhammadsaifalli222@gmail.com>
2023-09-19 09:50:16 +0200

    Calendar booking searhcing done

--- src/header.js
+++ src/header.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './header.css'; 
-import {RxHamburgerMenu} from 'react-icons/rx'
-import {BsPersonCircle} from 'react-icons/bs'
-import {AiOutlineSearch} from 'react-icons/ai'
+import './header.css';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { BsPersonCircle } from 'react-icons/bs';
+import { AiOutlineSearch } from 'react-icons/ai';
+import BookingDatePicker from './booking'; // Import your Calendar component here
 
 function Header() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false); // State to control the calendar visibility
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
 
+  const toggleCalendar = () => {
+    setIsCalendarOpen(!isCalendarOpen);
+  };
 
+  const handleDateSelect = (startDate, endDate) => {
+    // Handle the selected start and end dates here
+    console.log('Start Date (in Header):', startDate);
+    console.log('End Date (in Header):', endDate);
+
+    // Close the date picker
+    setIsCalendarOpen(false);
+  };
 
   return (
     <div className="header">
@@ -20,21 +33,32 @@ function Header() {
         <img src="logo.png" alt="Logo" className="header__logo" />
       </div>
       <div className="header__middle">
-        
-      <button className="header__searchButton">
-      <div className="header__searchText">Search</div>
-      <div className="header__searchHolder">      
-      <div className="header__searchIcon">
-        <AiOutlineSearch />
-      </div>
-      </div>
-      </button>
+        {/* Add onClick to the search button to toggle the calendar */}
+        <button className="header__searchButton" onClick={toggleCalendar}>
+          <div className="header__searchText">Search</div>
+          <div className="header__searchHolder">
+            <div className="header__searchIcon">
+              <AiOutlineSearch />
+            </div>
+          </div>
+        </button>
+
+        {/* Conditionally render the Calendar component */}
+        {isCalendarOpen && (
+          <div className="datepicker-popup">
+            <div className="datepicker-popup-content">
+              <BookingDatePicker onDateSelect={handleDateSelect} />
+            </div>
+          </div>
+        )}
       </div>
       <div className="header__right">
-      <button 
-      className={`header__profileButton ${isPopupOpen ? 'active' : ''}`}
-      onClick={togglePopup}> <RxHamburgerMenu/> <BsPersonCircle className='.BsPersonCircle'/>
-      </button>
+        <button
+          className={`header__profileButton ${isPopupOpen ? 'active' : ''}`}
+          onClick={togglePopup}
+        >
+          <RxHamburgerMenu /> <BsPersonCircle className=".BsPersonCircle" />
+        </button>
         {isPopupOpen && (
           <div className="header__popup">
             <Link to="/login">
