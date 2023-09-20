8da64480c9d753a4a6c7dce437d3947d09bc2712
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-09-20 16:44:33 +0200

    Fixed some issues on popup

--- src/propertyPopup.js
+++ src/propertyPopup.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './propertyPopup.css'; // Import the CSS file for styling
 
-
 function PropertyPopup({ property, onClose }) {
-    // This component receives the selected property as a prop and a function to close the popup
-  
-    return (
-      <div className="property-popup">
-        <div className="popup-content">
+  // This component receives the selected property as a prop and a function to close the popup
+
+  return (
+    <div className="property-popup">
+      <div className="popup">
+        <div className="close-holder">
           <button className="close-button" onClick={onClose}>
             X
           </button>
+        </div>
+        <div className="popup-content">
           <div className="image-holder">
-          <img src={property.imageUrl} alt={property.title} />
+            <img src={property.imageUrl} alt={property.title} />
           </div>
-          <h3>{property.title}</h3>
-          <p>{property.location}</p>
-          <p>{property.price}</p>
+          <h3 className="property-title">{property.title}</h3>
+          <p className="property-location">{property.location}</p>
+          <p className="property-price">{property.price}</p>
           {/* Add more property details here */}
         </div>
       </div>
-    );
-  }
-  
-  export default PropertyPopup;
+    </div>
+  );
+}
+
+export default PropertyPopup;
