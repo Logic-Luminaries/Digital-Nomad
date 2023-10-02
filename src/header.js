5c740fa99d90665c9c1ffa2643b4710bfc040438
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-10-02 17:33:26 +0200

    dates replaces search on click

--- src/header.js
+++ src/header.js
@@ -35,11 +35,19 @@ function Header({onDateSelect}) {
 
   return (
     <div className="header">
-      <div className="header__left">
-        <img src="logo.png" alt="Logo" className="header__logo" />
-      </div>
-      <div className="header__middle">
-        {/* Add onClick to the search button to toggle the calendar */}
+    <div className="header__left">
+      <img src="logo.png" alt="Logo" className="header__logo" />
+    </div>
+    <div className="header__middle">
+      {isCalendarOpen ? (
+        // Render the datepicker when the calendar is open
+        <div className="datepicker-popup">
+          <div className="datepicker-popup-content">
+            <BookingDatePicker onDateSelect={handleDateSelect} />
+          </div>
+        </div>
+      ) : (
+        // Render the search button when the calendar is closed
         <button className="header__searchButton" onClick={toggleCalendar}>
           <div className="header__searchText">Search</div>
           <div className="header__searchHolder">
@@ -48,39 +56,31 @@ function Header({onDateSelect}) {
             </div>
           </div>
         </button>
-
-        {/* Conditionally render the Calendar component */}
-        {isCalendarOpen && (
-          <div className="datepicker-popup">
-            <div className="datepicker-popup-content">
-              <BookingDatePicker onDateSelect={handleDateSelect} /> {/* call bookingdate picker and hanlde the dates*/}
-            </div>
-          </div>
-        )}
-      </div>
-      <div className="header__right">
-        <button
-          className={`header__profileButton ${isPopupOpen ? 'active' : ''}`}
-          onClick={togglePopup}
-        >
-          <RxHamburgerMenu /> <BsPersonCircle className=".BsPersonCircle" />
-        </button>
-        {isPopupOpen && (
-          <div className="header__popup">
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-            <Link to="/register">
-              <button>Sign Up</button>
-            </Link>
-            <Link to="/">
-              <button>About</button>
-            </Link>
-          </div>
-        )}
-      </div>
+      )}
     </div>
-  );
+    <div className="header__right">
+      <button
+        className={`header__profileButton ${isPopupOpen ? 'active' : ''}`}
+        onClick={togglePopup}
+      >
+        <RxHamburgerMenu /> <BsPersonCircle className=".BsPersonCircle" />
+      </button>
+      {isPopupOpen && (
+        <div className="header__popup">
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/register">
+            <button>Sign Up</button>
+          </Link>
+          <Link to="/">
+            <button>About</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  </div>
+);
 }
 
 export default Header;
